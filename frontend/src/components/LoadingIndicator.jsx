f6a244324a1e3f52fdd267be5337af6e9c9cfffb
medineshkumarm <medineshkumarm@gmail.com>
2024-03-27 14:41:10 +0530

    first commit

---
+++ frontend/src/components/LoadingIndicator.jsx
@@ -0,0 +1,10 @@
+import "../styles/LoadingIndicator.css"
+function LoadingIndicator() {
+  return (
+    <div className='loading-container'>
+        <div className='loader'></div>
+    </div>
+  )
+}
+
+export default LoadingIndicator
---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+function NotFound() {
+  return (
+    <div>
+      <h1>404 Not Found</h1>
+      <p>The Page you are for is not present</p>
+      </div>
+  )
+}
+
+export default NotFound
